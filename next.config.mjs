44a1490d1b8d69c0cfc9ea1dd103eed947d90f1e
emSoumik <soumik.halder026@gmail.com>
2024-11-26 11:24:18 +0530

    Enhance webpack configuration for video file handling and add new button styles in global CSS

--- next.config.mjs
+++ next.config.mjs
@@ -7,14 +7,17 @@ const nextConfig = {
   },
   basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : '',
-  // Add webpack configuration for video files
+  // Add webpack configuration for video files only
   webpack: (config) => {
     config.module.rules.push({
       test: /\.(mp4|webm)$/,
       use: {
         loader: 'file-loader',
         options: {
+          name: 'static/media/[name].[hash].[ext]',
           publicPath: `${process.env.NODE_ENV === 'production' ? '/portfolio/' : '/'}`,
+          quality: 100,
+          limit: 1024 * 1024 * 10,
         },
       },
     });
